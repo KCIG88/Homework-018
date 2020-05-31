06a3910a784e3ce165eccfa080d52f1f34e89404
kevin cox <kevincox1983@gmail.com>
2020-05-31 00:40:44 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 
@@ -15,9 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE || "mongodb://localhost/budget";
-
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
